5237f055741bba7509565425ffe727be3d38ecd6
nicholaslee119 <nicholaslee119@gmail.com>
2019-02-26 20:02:52 +0900

    remove unused file

--- android/src/main/java/com/reactnativecommunity/checkbox/CheckboxPackage.java
+++ android/src/main/java/com/reactnativecommunity/checkbox/CheckboxPackage.java
@@ -10,7 +10,8 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNCReactNativeCheckboxPackage implements ReactPackage {
+
+public class CheckboxPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNCReactNativeCheckboxModule(reactContext));
