0f726b51c1a63c032bcd31592f05b0c014d17ab2
nicholaslee119 <nicholaslee119@gmail.com>
2019-04-05 16:31:36 +0900

    activiate unit test

---
+++ js/__test__/CheckBox.test.js
@@ -0,0 +1,38 @@
+/* @flow */
+
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import Checkbox from '../CheckBox';
+
+ describe('<Checkbox />', () => {
+  it('renders enabled Checkbox', () => {
+    const tree = renderer
+      .create(
+        <Checkbox />
+      )
+      .toJSON();
+
+     expect(tree).toMatchSnapshot();
+  });
+
+   it('renders disabled Checkbox', () => {
+    const tree = renderer
+      .create(
+        <Checkbox disabled={true} />
+      )
+      .toJSON();
+
+     expect(tree).toMatchSnapshot();
+  });
+
+   it('renders Checkbox with value', () => {
+    const tree = renderer
+      .create(
+        <Checkbox value={1} />
+      )
+      .toJSON();
+
+     expect(tree).toMatchSnapshot();
+  });
+});
+  
