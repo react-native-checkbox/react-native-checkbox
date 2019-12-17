aa3c26053229df349b373ee54b540ddff96e23a8
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-12-17 15:51:18 +0900

    chore: reaplce example with CheckboxExample

--- example/ios/CheckboxExampleTests/CheckboxExampleTests.m
+++ example/ios/CheckboxExampleTests/CheckboxExampleTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface exampleTests : XCTestCase
+@interface CheckboxExampleTests : XCTestCase
 
 @end
 
-@implementation exampleTests
+@implementation CheckboxExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
