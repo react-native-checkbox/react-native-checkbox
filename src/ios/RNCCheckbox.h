d4ed3d595f4bd846d48604a1cda9c14a364c8313
nicholaslee119 <nicholaslee119@gmail.com>
2020-05-09 22:02:16 +0900

    adjust name of event

--- src/ios/RNCCheckbox.h
+++ src/ios/RNCCheckbox.h
@@ -6,12 +6,11 @@
 //  Copyright © 2020 Facebook. All rights reserved.
 //
 #import "BEMCheckBox.h"
-
 #import <React/RCTComponent.h>
 
 @interface RNCCheckbox: BEMCheckBox
 
 @property (nonatomic, copy) RCTBubblingEventBlock onAnimationDidStop;
-@property (nonatomic, copy) RCTBubblingEventBlock onDidTap;
+@property (nonatomic, copy) RCTBubblingEventBlock onValueChange;
 
 @end
--- src/ios/RNCCheckboxManager.h
+++ src/ios/RNCCheckboxManager.h
@@ -5,6 +5,7 @@
 //  LICENSE file in the root directory of this source tree.
 
 #import <React/RCTViewManager.h>
+#import "BEMCheckBox.h"
 
 @interface RNCCheckboxManager : RCTViewManager <BEMCheckBoxDelegate>
 
