022df55be9ac5a174f9f2cb234366bf899b8681e
nicholaslee119 <nicholaslee119@gmail.com>
2020-09-08 12:23:05 +0900

    fix typings refer bug

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,8 +1,8 @@
 import {Component} from 'react';
 import {Constructor, NativeMethodsMixin} from 'react-native';
-import {Props as AndroidProps} from '../js/CheckBox.android';
-import {Props as IOSProps} from '../js/CheckBox.ios';
-import {Props as WindowsProps} from '../js/CheckBox.windows';
+import {Props as AndroidProps} from '../dist/CheckBox.android';
+import {Props as IOSProps} from '../dist/CheckBox.ios';
+import {Props as WindowsProps} from '../dist/CheckBox.windows';
 
 type CheckBoxProps = AndroidProps & IOSProps & WindowsProps;
 
