ace969b39f38f5cac52fbef0685765c9cc94bd90
nicholaslee119 <nicholaslee119@gmail.com>
2020-09-10 11:16:10 +0900

    adjust comment

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@
  */
 import {AppRegistry} from 'react-native';
 import App from './App';
-// You could alse use the function component
+// You could use the function component as well
 // import App from './AppFunction';
 import {name as appName} from './app.json';
 
