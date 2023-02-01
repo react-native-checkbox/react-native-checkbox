f1ea89eb3de328e029bf4660d072133c5e1890fc
Minsik Kim <kmsbernard@gmail.com>
2023-02-01 15:06:12 +0900

    fix(types): utility type removed from RN 0.71

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,12 +1,14 @@
 import {Component} from 'react';
-import {Constructor, NativeMethodsMixin} from 'react-native';
+import {NativeMethods} from 'react-native';
 import {Props as AndroidProps} from '../dist/CheckBox.android';
 import {Props as IOSProps} from '../dist/CheckBox.ios';
 import {Props as WindowsProps} from '../dist/CheckBox.windows';
 
+type Constructor<T> = new (...args: any[]) => T;
+
 type CheckBoxProps = AndroidProps & IOSProps & WindowsProps;
 
 declare class CheckBoxComponent extends Component<CheckBoxProps> {}
-declare const CheckBoxBase: Constructor<NativeMethodsMixin> &
+declare const CheckBoxBase: Constructor<NativeMethods> &
   typeof CheckBoxComponent;
 export default class CheckBox extends CheckBoxBase {}
